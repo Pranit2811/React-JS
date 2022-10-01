ae7467dec0e3387fa0e28ce2083706fa8fd639aa
Pranit Raut <pranit.raut@routemobile.com>
2022-10-01 19:49:55 +0530

    Adding + Auto Dismissing Alert Messages | Complete React Course in Hindi #13

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,25 +4,41 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import { useState } from 'react';
+import Alert from './components/Alert';
 function App() {
   const [mode, setMode] = useState('light')
   const [text, setText] = useState('dark')
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message,type) => {
+    console.log(message,type)
+      setAlert({
+          message :message,
+          type :type
+                })
+      setTimeout(() => {
+        setAlert(null)
+      }, 2000);
+               
+  }
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark')
       setText('light ')
-      document.body.style.backgroundColor = 'grey'
+      document.body.style.backgroundColor = '#042743'
+      showAlert('Dark Mode has been enabled','success')
     }
     else {
       setMode('light')
       setText('dark')
       document.body.style.backgroundColor = 'white'
+      showAlert('Light Mode has been enabled','success')
     }
   }
   return (
     <>
       <Navbar title='Text-Utils' mode={mode} toggleMode={toggleMode} text={text} />
-      <div className="container"><TextForm heading="Enter Your Text To Analyse" mode={mode} /></div>
+      <Alert alert={alert}/>
+      <div className="container" ><TextForm heading="Enter Your Text To Analyse" showAlert={showAlert} mode={mode} /></div>
       {/* <About/> */}
     </>
   );
